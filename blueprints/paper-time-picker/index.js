1582ee1a77de45db6db4b2aead6be20c2ed26d06
Devonte <devo.tox.89@gmail.com>
2017-06-01 14:50:21 +0100

    normalize entity name

--- blueprints/paper-time-picker/index.js
+++ blueprints/paper-time-picker/index.js
@@ -1,5 +1,10 @@
 /* eslint-env node */
 module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
   afterInstall: function() {
     return this.addPackagesToProject([
       {name: 'ember-paper'},
