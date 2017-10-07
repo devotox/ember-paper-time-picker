11354804d03706b384e8c21e104bb2e8dcca2f19
Devonte <devo.tox.89@gmail.com>
2017-10-07 10:54:37 +0100

    update

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/paper-time-picker/';
+    ENV.rootURL = '/ember-paper-time-picker/';
     // here you can enable a production-specific feature
   }
 
