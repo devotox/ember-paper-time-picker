47c979aaa490b0b8aa875ed2328723cb7706f6aa
Devonte <devo.tox.89@gmail.com>
2017-10-11 01:38:05 +0100

    Deploy gh-pages from commit be6eb4a549d631fb5bed00a5bcd5fbc79b82eee4

--- assets/vendor.js
+++ assets/vendor.js
@@ -5594,7 +5594,7 @@ var n=Ember.getOwner}),define("moment/index",["exports","moment/lib"],function(e
 function n(e,n){if(t.default.isMoment(e)&&t.default.isMoment(n))return e.isBefore(n)?-1:e.isSame(n)?0:1
 throw new Error("Arguments provided to `compare` are not moment objects")}Object.defineProperty(e,"__esModule",{value:!0}),t.default.prototype.compare=n,t.default.compare=n,t.default.prototype.clone=function(){return(0,t.default)(this)},e.default=t.default}),define("moment/lib",["exports"],function(e){"use strict"
 Object.defineProperty(e,"__esModule",{value:!0}),e.default=self.moment}),define("paper-time-picker/components/paper-time-picker",["exports","moment","ember-paper/components/paper-input","ember-paper/templates/components/paper-input"],function(e,t,n,r){"use strict"
-Object.defineProperty(e,"__esModule",{value:!0}),e.default=n.default.extend({layout:r.default,defaultOptions:{interval:60,dynamic:!1,dropdown:!0,scrollbar:!0,minTime:"12:00am",maxTime:"11:00pm",startTime:"12:00am",timeFormat:"h:mm p",momentFormat:"h:mm a"},didInsertElement:function(){var e=this
+Object.defineProperty(e,"__esModule",{value:!0}),e.default=n.default.extend({layout:r.default,defaultOptions:{interval:60,dynamic:!1,dropdown:!0,scrollbar:!1,minTime:"12:00am",maxTime:"11:00pm",startTime:"12:00am",timeFormat:"h:mm a",momentFormat:"h:mm a"},didInsertElement:function(){var e=this
 this._super.apply(this,arguments),this.set("options",Object.assign(this.get("defaultOptions"),this.get("options"),{change:function(n){n=n?(0,t.default)(n).format(e.options.momentFormat):"",e.sendAction("onChange",n)}}))
 var n=this.get("options"),r=this.$("input").timepicker(n)
 this.set("timepicker",r)},willDestroyElement:function(){this._super.apply(this,arguments)
